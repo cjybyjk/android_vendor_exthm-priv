c43842edaa9334982f40466b5b1d0190e759c222
cjybyjk <cjybyjk@gmail.com>
2019-12-25 00:23:27 +0800

    exthm-priv: fix MiPush

---
+++ config.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+VENDOR_PATH := vendor/exthm-priv
+
+# MiPush
+PRODUCT_PACKAGES += \
+	MiPush \
+    MiPushService
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.miui.ui.version.name = V10 \
+    ro.miui.ui.version.code = 8 \
+    ro.miui.version.code_time = 1544025600 \
+    ro.fota.oem = Xiaomi \
+    ro.miui.internal.storage = /sdcard/
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc/permissions,system/etc/permissions)
